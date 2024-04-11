bcbb8de9c3b3dae9fd350e13b4bab146708fd0de
razibraj <razibraj56@gmail.com>
2024-04-11 20:40:18 +0600

    create navber menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,43 @@
+"use client"
+
 import Link from "next/link"
+import { useState } from "react";
+
+
+const links = [
+    { url: "/", title: "Home" },
+    { url: "/about", title: "About" },
+    { url: "/portfolio", title: "Portfolio" },
+    { url: "/contact", title: "Contact" }
+]
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
             <div>
-                <Link href="/" className="text-sm bg-black rounded-md p-1">
-                    <span className="text-white">Lama</span>
-                    <span className="w-12 h-8 rounded bg-white text-black">.dev</span>
+                <Link href="/" className="text-sm bg-black rounded-md p-1
+                flex items-center justify-center">
+                    <span className="text-white mr-2">Lama</span>
+                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">.dev</span>
                 </Link>
             </div>
-            <div>2</div>
+            {/* responsive menu */}
+            <div>
+                <button className="w-10 h-8 flex flex-col justify-between relative z-50">
+                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <div className="w-10 h-1 bg-white rounded"></div>
+                </button>
+                {/* menu list */}
+                {open && (
+                    <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8">
+                        {links.map(link => (
+                            <Link href={link.url} key={link.title}>{link.title}</Link>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
