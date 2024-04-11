cd76a9d76f2f2fc35b264eacdd68a091d1f94fac
razibraj <razibraj56@gmail.com>
2024-04-11 20:05:56 +0600

    design the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-
+import Link from "next/link"
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Heeollo world</h1>
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+            <div>
+                <Link href="/" className="text-sm bg-black rounded-md p-1">
+                    <span className="text-white">Lama</span>
+                    <span className="w-12 h-8 rounded bg-white text-black">.dev</span>
+                </Link>
+            </div>
+            <div>2</div>
         </div>
     );
 };
