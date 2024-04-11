29d606d3d440fe423e418807da6419c3b468b66e
razibraj <razibraj56@gmail.com>
2024-04-12 00:09:42 +0600

    add photo

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,25 @@
 import Image from "next/image";
 
-export default function Home() {
+const Homepage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div>
-          {/* image container */}
-          <div>
-            <Image src="/hero.png" alt="" fill className="object-container" ></Image>
-          </div>
-          {/* text container */}
-        </div>
-       
-      </div>
 
-   
-     
-    </main>
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* image container */}
+      <div className="h-1/2 relative lg:w-1/2 lg:h-full">
+        <Image src="/hero.png" alt="" fill className="object-contain" ></Image>
+      </div>
+      {/* text container */}
+      <div className="w-1/2 lg:w-1/2 lg:h-full flex flex-col gap-8 items-center
+          justify-center">
+        <h1 className="text-4xl md:text-5xl font-bold">Explore the world, learn new things, embrace change, and cherish every moment life offers with gratitude.</h1>
+        <p className="md:text-xl">Explore the world, learn new things, embrace change, and cherish every moment life offers with gratitude.cherish every moment life offerscherish every moment life offers</p>
+        <div className="flex gap-4">
+          <button className="btn bg-slate-600">kkkk</button>
+          <button className="btn bg-slate-500">kkkk</button>
+        </div>
+      </div>
+    </div>
   );
 }
+
+export default Homepage;
